ded382c922de7d0938e45a01736c97ecd9ef7c14
AndysMac <andyliu3411@gmail.com>
2024-01-18 15:20:22 -0500

    variable rendering

--- app.py
+++ app.py
@@ -12,10 +12,14 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.urandom(64)
 
+@app.template_filter(name='linebreaker')
+def linebreaksbr_filter(text):
+    return text.replace('\n', '<br>')
+
 # Endpoints/Routes
 @app.route('/')
 def index():
-    return render_template("data.html")
+    return render_template("index.html")
 
 @app.route('/login')
 def login():
@@ -35,7 +39,7 @@ def login():
         auth_url = auth_manager.get_authorize_url()
         return redirect(auth_url)
 
-    spotify = spotipy.Spotify(auth_manager=auth_manager) # if signed in already, sends to data page
+    spObject = spotipy.Spotify(auth_manager=auth_manager) # if signed in already, sends to data page
     return redirect('/data')
 
 @app.route('/data')
@@ -43,18 +47,61 @@ def data():
     cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
     auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
 
-    sp = spotipy.Spotify(auth_manager=auth_manager)
+    spObject = spotipy.Spotify(auth_manager=auth_manager)
 
-    ranges = ['short_term', 'medium_term', 'long_term']
-    result_string = ''
+    ranges = ['short_term', 'medium_term','long_term']
+    sTracks = []  # Initialize an empty list to store the result strings
+    mTracks = []
+    aTracks = []
+    
 
-    for sp_range in ranges:
-        result_string += f"range: {sp_range}\n"
-        results = sp.current_user_top_tracks(time_range=sp_range, limit=50)
-        for i, item in enumerate(results['items']):
-            result_string += f"{i} {item['name']} // {item['artists'][0]['name']}\n"
-    return result_string
+    for spObject_range in ranges:
+        results = spObject.current_user_top_tracks(time_range=spObject_range, limit=25)
+        ctr = 1
+        for item in results['items']:
+            track_info = {
+                'number': ctr,
+                'image': item['album']['images'][0]['url'],
+                'name': item['name'],
+                'artist': item['artists'][0]['name']
+            }
+            if spObject_range == 'short_term':
+                sTracks.append(track_info)
+            elif spObject_range == 'medium_term':
+                mTracks.append(track_info)
+            elif spObject_range == 'long_term':
+                aTracks.append(track_info)
+            ctr += 1
+        
+    return render_template("data.html", sTracks=sTracks, mTracks=mTracks, aTracks=aTracks)
 
-@app.route('/tracks')
-def topTracks():
-    return
+@app.route('/artists')
+def topArtists():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+
+    spObject = spotipy.Spotify(auth_manager=auth_manager)
+
+    sArtists = []  # Initialize an empty list to store the result strings
+    mArtists = []
+    aArtists = []
+
+    for spObject_range in ['short_term', 'medium_term', 'long_term']:
+        results = spObject.current_user_top_artists(time_range=spObject_range, limit=25)
+        ctr = 1
+        for item in results['items']:
+            artist_info = {
+                'number': ctr,
+                'name': item['name'],
+                'image': item['images'][0]['url'],
+            }
+            if spObject_range == 'short_term':
+                sArtists.append(artist_info)
+            elif spObject_range == 'medium_term':
+                mArtists.append(artist_info)
+            elif spObject_range == 'long_term':
+                aArtists.append(artist_info)
+    return render_template("artists.html", sArtists=sArtists, mArtists=mArtists, aArtists=aArtists)
+
+if __name__ == '__main__':
+    app.run(debug=True)
